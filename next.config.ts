9c713237cffb9f5e4f58dafbe737eb87a9fca377
AlexanderBits <vendas@churrasqueirasdetijolos.com.br>
2026-04-07 13:21:53 -0300

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
+  output: "export",
+  basePath: "/Marluce-Cunha-Pijamas",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
